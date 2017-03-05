f1724c1f214ff972220d768ab32ee93eaf07da8b
Spencer Bryson <spencer.bryson@uoit.net>
2017-03-05 00:29:14 -0500

    Added POST method for getting raw HTML for bookings.

--- app/src/main/java/com/example/spenc/uoitstudyroom/DataScraper.java
+++ app/src/main/java/com/example/spenc/uoitstudyroom/DataScraper.java
@@ -10,10 +10,10 @@ import java.net.Socket;
 import javax.net.ssl.SSLSocketFactory;
 
 /**
- * Created by Spenc on 2017-03-03.
+ * Scrapes various sorts of needed data from UOIT's library website
  */
 
-public class DataScraper {
+class DataScraper {
 
     private Socket socket = null;
 
@@ -28,7 +28,7 @@ public class DataScraper {
     }
 
     private void disconnect() {
-        if(!socket.isClosed()) {
+        if(socket != null && !socket.isClosed()) {
             try {
                 this.socket.close();
             } catch(IOException e) {
@@ -37,11 +37,8 @@ public class DataScraper {
         }
     }
 
-    public boolean probeSocket() {
-        if(this.socket != null)
-            return this.socket.isConnected();
-        else
-            return false;
+    private boolean probeSocket() {
+        return this.socket != null && this.socket.isConnected();
     }
 
     public char[] getRawHtml() {
@@ -49,7 +46,7 @@ public class DataScraper {
 
         connect();
 
-        if(this.socket.isConnected()) {
+        if(probeSocket()) {
             try {
                 // Send a GET header to the HTTP server to receive HTML data of the page we want
                 PrintWriter pw = new PrintWriter(this.socket.getOutputStream());
@@ -92,7 +89,92 @@ public class DataScraper {
 
                 br.close();
 
-                //System.out.println("Bytes Read: " + total_bytes_read);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            //TODO: Handle no connection
+            System.out.println("[ERROR] No connection established!");
+        }
+
+        disconnect();
+
+        return cbuf;
+    }
+
+    char[] postDate(int dateID, String[] formData) {
+        // ASP.NET form-data
+        String eTarget = formData[0];
+        String vState = formData[1];
+        String vStateGen = formData[2];
+        String eValidation = formData[3];
+        String eArgument = Integer.toString(dateID);
+        String boundary = "----BookingBoundary7MA4YWxkTrZu0gW";
+        String cr = "\r\n";
+        String cd = "Content-Disposition: form-data; ";
+
+        // Create data payload for ASP.NET form
+        String payload =
+                "--" + boundary + cr + cd + "name=\"__EVENTTARGET\"\r\n\r\n" + eTarget + cr +
+                "--" + boundary + cr + cd + "name=\"__EVENTARGUMENT\"\r\n\r\n" + eArgument + cr +
+                "--" + boundary + cr + cd + "name=\"__VIEWSTATE\"\r\n\r\n" + vState + cr +
+                "--" + boundary + cr + cd + "name=\"__VIEWSTATEGENERATOR\"\r\n\r\n" + vStateGen + cr +
+                "--" + boundary + cr + cd + "name=\"__EVENTVALIDATION\"\r\n\r\n" + eValidation + cr +
+                "--" + boundary + "--";
+
+        int cLength = payload.length();
+
+        char[] cbuf = null;
+
+        connect();
+
+        if(probeSocket()) {
+            try {
+                // Send a GET header to the HTTP server to receive HTML data of the page we want
+                PrintWriter pw = new PrintWriter(this.socket.getOutputStream());
+                pw.print("POST /uoit_studyrooms/calendar.aspx HTTP/1.1\r\n");
+                pw.print("Host: rooms.library.dc-uoit.ca\r\n");
+                pw.print("User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64)\r\n");
+                pw.print("Content-Type: multipart/form-data; boundary=----BookingBoundary7MA4YWxkTrZu0gW\r\n");
+                pw.print("Content-Length: " + cLength + "\r\n\r\n");
+
+                pw.print(payload);
+
+                pw.flush();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+
+                String line;
+                int content_length = 0;
+                int max_iter = 30;
+
+                // Get length of content being received
+                for(int i = 0; i < max_iter; i++) {
+                    line = br.readLine();
+                    if (line.contains("Content-Length:")) {
+                        String num = line.substring(16);
+                        content_length = Integer.parseInt(num);
+                        break;
+                    }
+                }
+
+                // No data was received
+                if(content_length == 0)
+                    System.out.println("[ERROR] No suitable content found.");
+                //TODO: inform user of possible connection issue
+
+                cbuf = new char[content_length];
+
+                int total_bytes_read = 0;
+
+                while(content_length > 0) {
+                    int bytes_read = br.read(cbuf, total_bytes_read, content_length);
+                    content_length -= bytes_read;
+                    total_bytes_read += bytes_read;
+                }
+
+                br.close();
+
             } catch(IOException e) {
                 e.printStackTrace();
             }
--- app/src/main/java/com/example/spenc/uoitstudyroom/ScrapeBookingsTask.java
+++ app/src/main/java/com/example/spenc/uoitstudyroom/ScrapeBookingsTask.java
@@ -7,13 +7,23 @@ import java.io.IOException;
  * Scrapes raw HTML data from website, needing to be parsed
  */
 
-public class ScrapeBookingsTask extends AsyncTask<DataScraper, Void, Integer> {
+class ScrapeBookingsTask extends AsyncTask<DataScraper, Void, Integer> {
 
     @Override
     protected Integer doInBackground(DataScraper... param) {
-        DataScraper dataScraper = (DataScraper) param[0];
+        //TODO: Do we really need to have the scraper exist outside of a task?
+        DataScraper dataScraper = param[0];
 
-        char[] cbuf = dataScraper.getRawHtml();
+        String[] formData = new String[4];
+
+        formData[0] = "ctl00$ContentPlaceHolder1$Calendar1";
+        formData[1] = "QI916RYBbfQD6nuE3P2gUrvDb1BDJVQtoegI6o9TQULjip291oACQAghC9JgAqkrAer78ueTRqk4Okz0QU77V557+IiU/xKFi73fvkLyGACRqDhgbxAAdBxfePQYalNCXAV3QweGJdAtRy1aLFkvqIAJmPEWC9HhWtziqyN8XgOZb6jvRHUQyjsvqsahMKGGWE9kcxuOThDTg1IaNlo7PYXwQd0TkifIrEg4L6/4iis01RMmAZKPFGO+MHE+LR6HfxSSSfVih0ygC2aBNQDAu5uasnLD3lj3Pl7X8NEg4mKOjo29/xcptjMoGRWlS0Hu7ZjS/KMjvKr9k/+OiWHNu25cez40rszFn4d2y6KPWGc/kwwHAt/egUzGv4DdLJNn6o/K2Iz3TPxmFHHuTBEujcSD1fC4LdrWpfHHJg+9n70ZRd86npeSPD1DY990zOSr/EZY0ZuQApmW3PqYlAVwxolybHrsMgmuTXEsO/B2u1/amSK5H641g8pHv2i80zf3djGrozbmp1ZLWm6M8MP32sJGCSmlTA5jBQdFZKputn1Fzlk0LHdUoKm7Ty1NYA7lTrXGc5sRe4nfAI7QAbDUmGiSX3mJnNuK0p038ahoOTQ4zCXCEGvN4tKndRmOXSTb3no+f2IyY5uFsKJL0WQQjC0NHurmpNvdMTFhd2vux201D7IvIHI8SYtRZztzwqKaSmiipdKZQTsOC903ErpPrNPIy3Mw/RHP0brwuzEJ3ZmjPlLMsxszbV65esBpwSS8DztH3aWkU52CG7aZSHtKN2XS9Mm7C3iF/W30ddEk3PzMC7gsWAQ+v6jClEz9yUMahcdkZJq1zgzuq2mKyEDkEzhBvSxUvHyJ7ggwWQneEXFa1LxuC2T1jiuunCYODBtdhZbg0yBjJAECSHEfx0AbdfhdJ2lfFXJPATS466uPeKIqtVOdrwRWisBl+lOHdRjKPPmW8XazcclO13ap44v+S1GOlxXsEBTxTVnzxA+XTTs1yOELlzOkOqiteY5mmy0tZ4CK2ACU/JMVrpIDbtxZ+CMEPjwqQln+mooX0NVrdAe6CHu+VxrnL/CKPy5n8Jr0xEpbmVEaPU1Cjx1ejeUme92eNzdzZ8MBMCPrxQ/AsH2VaFozPXYQb0P4WBSb9Kedp06Gl/HbwzhWiknrBlXZLL8TpqdoB11G9tERr7CUAullEBpdFdCrQBvEFZmL99un90w497c1s5lc0SVJJpSSfg==";
+        formData[2] = "80536060";
+        formData[3] = "Ptb1rYxaXnc9TOf07fS94ZNi36RioOLkPv4yPSbSlWAd+frDycynM1TTgeg/t7qTfG7qieF3+iiuJk9EBcrpTUDvcYAOQMvrIyWljNR+kZOz/AZ5SL2jysacl1DG3hR7yiLR2dVx4bQ94jhmKmJ8mUk7VBEoM7/RVxt22iMk0N6hgGnZyURlHrhDsineyzjsQxk14FmXMF1H6vNwenHuZFNdFbu1G+n9FrQs/ZyK8WmiIJWkNmcai6b9VtJ8KOEqgaZdm2U/61G7N7Ft8lncqXFj4CPyQ0EmRYXKCfYM2LlQTSGEOidAK8kdh/e8yPRQkpiHez/irAfS8s9ayR7FNapCozalAGNynoTe7v/i5mp4Mz1eyyr700BIRexByc/QoCgy2D9qaE4f772hgndOEnFx401T1T5ybqf1p3vzSJ4Zhi2iM1WJqtiniGD7c5e7QOUHRUF42HSQaFjkw2En3Hn4VkEVHNN6/BHmQEDNFcrrMyleoWNwQnY0TtyfIPUGQOZzPPwmBtuSt3xrfAVaBKAaJZqAj0m2a9bID88klQuKB/xJZolKu4XWKdDUk3cl9HPNXcHgIcUFz9kiUBgprh3NACRoE1avpEns2BRL9Qd1Q4KppVXbq+3Mcy2mr2hndjFh7rTK/Dj27jyeuROkHG4scJEF5bbaNezt+AWUVwG1kWwzl6/LQKRq0FZP1vkxjBOzX+ZruQL/mzDbc0JPXLZxgwi86/x+9jC8YWBxpfGRMzvh+xn9HjOUG3/JZWplaLeq1tTGn5M0VRCkc2+uMQi5q4pLf3W60+MNByahgiQuSg0o2hmqah8ucRQtCnpXtWzwGkvWzrxGXBPnQpWjYa/XUWx7SBB36yk67K9CLCFQg1wiklKj2TzggNb26rRFFrP5HxU0XKii6jwG4cTM7xK76uWZkDn+OeCyBiMBo/3IXmph5Zjw9bsuotFf6bCmujIsf9ua7lAPiyascmFHFuSBbC5a9C0NOmrUwb278p588xX4QaurTNDPIz+aIA2TtOL7j/jHhBBvv9GIF5W3WlGsN8RZkoJ3SerRhp5tcaX0YzG6ukbJuELTDHN4LrnrN/E94haYvDam9QWbg6CQ8tt6wY4gVcl1kaWrb2eMNcbmGuKrBhyvZpvadQInCDLqFow/lE3q6o7ePqdRo9pEzrchGcmzIB6C3QemFiFpk4gyGedvoGUXY2ooTe6oZWYYk1ongZJj7MDQiokk6qSGiDGH4rqOrW5FoxM2WSwQIqo/zilBrvplVfZObw18vT+HPbfcrxteR+HxKgydwgw05h1lsK40sFP2TxLn2WnS7W7JnJ6f/b4jzDrBiEJNoT0z4QGv6KB2EZtcQAQs/Dk6M01SiHLxo6vG4B28izY8PowGzf9JZkN6Pmhz7QaqLXfQscb3OaIfv2oRzWiCPdYWr20AZ2zkNFQQ4SmclTwWc/UXk68hPbKyWtW5ZkM8pGP9NzKMQbrdS6inHJFMQWHWoIv6CqdDFsyIUI2q4iPVdNlIj5kotE3RoT6Ya0sGLposzwn3Lzyb1pPBiQFuMHtDT0ykuwA9c/+1wPitI7LLg0wd/T1bh/Y4N5ZL1zExa0Kf1+F7DMgOnHNg0y2CAHFKgxMSWsKG6dWvHXvNpitJ9uK/x2Kcy2AhZh7U7wGAqQ3LunFu2Oyc/0gFGjmN+iElqS7NKtSX+tpV4+l2dxbpfS3Kxoj/AX+wBIff5rkrrcs56Mf1oCjZfezFYQCgdyguOC26f24T0sgnePkEtb2eBqk0ggxIdDb6sGSiRmAWVue1HhBIBkXU/FixAXhmmjbiBDEjds+oTV/qJM6DruRUvxmeNPBKfWCiPHqyJGn0v7PX2JiY74aQJrnsU36vzcjQsukgZldMGfxq6wiMhu3Gsys=";
+
+        char[] cbuf = dataScraper.postDate(6273,formData);
+
+        //System.out.println(new String(cbuf));
 
         // Raw HTML in cbuf will be passed to another class to parse it
 
