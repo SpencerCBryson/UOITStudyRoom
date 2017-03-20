236a195321fb7a1cbc78a6235081f3e0bd5ff143
DatLombardo <Michael.Lombardo@uoit.net>
2017-03-20 19:43:58 -0400

    Initialize UserInterface Branch / Added SplashScreen

---
+++ app/src/uiTest/UserInterface/app/src/main/java/com/example/michael/userinterface/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.michael.userinterface;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by michael on 20/03/17.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Handler handler = new Handler();
+        //Forces Main Activity to launch following 4000ms
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        },4000);
+    }
+}
